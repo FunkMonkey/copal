9d17120fcf03f01c321561a131030654af982075
AndSelm <and.selm+github@gmail.com>
2015-04-12 11:46:15 +0200

    - moved profile directory checking here and away from `copal-core`

--- copal.js
+++ copal.js
@@ -1,10 +1,15 @@
 "use strict";
 
+var path = require( "path" );
 var app = require( "app" );
 var CopalCore = require( "copal-core" );
 
 app.on( "ready", function() {
-  CopalCore.run().then( function() {
+
+	var appDataDir = process.env.APPDATA || ( process.platform === "darwin" ? process.env.HOME + "Library/Preference" : "/var/local" );
+	var defaultProfileDir = path.join( appDataDir, "copal" );
+
+  CopalCore.run( defaultProfileDir ).then( function() {
     console.log( "CoPal initialized" );
   } );
 });
